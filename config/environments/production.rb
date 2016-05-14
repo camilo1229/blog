8eb904fe76300e9c88a6275228b02aece9c73e17
Maikol Rico perez <mrico@unitecnica.net>
2016-05-14 12:44:51 -0500

    tercero

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {host: 'iunisoft.herokuapp.com'}
-  config.action_mailer.delivery_method = smtp
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
